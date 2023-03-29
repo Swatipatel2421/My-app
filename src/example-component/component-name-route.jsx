f8952949080d6c2cc34e320c5a882c1b53cbdcca
Swatipatel2421 <115700974+Swatipatel2421@users.noreply.github.com>
2023-03-29 14:09:44 +0530

    Update component-name-route.jsx

--- src/example-component/component-name-route.jsx
+++ src/example-component/component-name-route.jsx
@@ -3,13 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import CompoNameMenu from './component-name';
 import FunctionComponent from './function-component';
 
-const ClassCompoRoute = React.lazy(()=>{ return import('./class-component/classcomproute.jsx') });
+const ClassCompoRoute = React.lazy(()=>{ return import('./class-component/classcomproute.jsx') });// lazy loading  allows you to load parts of your application on-demand to reduce the initial load time
 
 class ComponentNameRoute extends Component {
     render() {
         return (
             <>
-                <Routes>
+                <Routes>//It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.
                     <Route path="/" element={<CompoNameMenu />} >
                         <Route path="classcomponent/*" element={<Suspense fallback={<h2>Loading...</h2>}><ClassCompoRoute/></Suspense>}></Route>
                         <Route path="functioncomponent" element={<FunctionComponent />} />
