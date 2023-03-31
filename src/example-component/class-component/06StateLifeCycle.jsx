fe45527a02f6db43373034e155b1cd59abda9876
Swatipatel2421 <115700974+Swatipatel2421@users.noreply.github.com>
2023-03-31 13:58:03 +0530

    Add files via upload

--- src/example-component/class-component/06StateLifeCycle.jsx
+++ src/example-component/class-component/06StateLifeCycle.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class StateLifeCycleCompo extends Component {
     render() {
@@ -25,8 +26,8 @@ class StateLifeCycleCompo extends Component {
                                 <li className='mb-1'><p><strong>ComponentWillUnmount&#40;&#41;</strong> - just run before unmounting method.</p></li>
                             </ul>
                         </li>
-                        
                     </ul>
+                    <Link to='../exampleLifeCycle'>Example</Link>
                 </div>
             </>
         );
